7773c2aaa62c17b72aa0687a4b2059b342caebf6
Isabel Pereira <isabelalvaro116@gmail.com>
2023-05-05 18:55:43 -0300

    feita a aula de metodos numericos

---
+++ 25_metodos_number/js/script.js
@@ -0,0 +1,23 @@
+// conversao de string para parsefloat
+console.log(parseFloat8('12.99'))
+console.log(Number.parseFloat('12.99'))
+
+// conversacao de string paraparsInt
+
+console.log(parseInt('10'))
+console.log(parseInt(16.96))
+
+// tofixed
+
+console.log(24.777557.toFixed(1))
+
+// isNaN
+
+console.log(isNaN('teste'))
+console.log(isNaN(12))
+console.log(isNaN('44'))
+
+//  o limite dos numeros que js aceita
+
+console.log(Number.MAX_VALUE) // quando o numero for maior q isso, dá infinity
+console.log(Number.MIN_VALUE)
